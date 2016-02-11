951a8971fa726ba9539ab164693e956b4c3f1967
Reza <srh.umir@gmail.com>
2016-02-12 00:36:39 +0100

    plot5 started

--- Plot6.R
+++ Plot6.R
@@ -17,20 +17,17 @@ NEI <- readRDS("./data/summarySCC_PM25.rds")
 SCC <- readRDS("./data/Source_Classification_Code.rds")
 
 
-##How have emissions from motor vehicle sources changed from 1999-2008
-##in Baltimore City?
-###subset to Baltimore
-NEIBal <- subset(NEI, fips == "24510")
-###find cehicle relared SCC's
-index <- grep(".*[Vv]ehicle.*", SCC$Short.Name)
-VehicleSCC <- as.character(SCC$SCC[a])
-NEIBalVehicle <- subset(NEIBal, SCC %in% VehicleSCC)
+##Compare emissions from motor vehicle sources in Baltimore City with emissions
+##from motor vehicle sources in Los Angeles County, California (fips == "06037").
+##Which city has seen greater changes over time in motor vehicle emissions?
 
+###subset to Baltimore
+NEIBalLos <- subset(NEI, fips == "24510" | fips == "06037")
+
+use qplot with weight and aes maybe density
+qplot(year, weight = Emissions, data = NEIBalLos, col = fips)
 ##start plotting
 png("./assignment2/plot5.png", type = "cairo")
-plot(unique(NEIBalVehicle$year),tapply(NEIBalVehicle$Emissions, NEIBalVehicle$year, sum),
-     type = "l", lwd = 3, col = "blue",
-     main = "", xlab = "", ylab = "")
-title(main = "Total PM.25 emission related to Vehicle",
-      xlab = "Year", ylab = "PM2.5 emission (ton)")
+
+
 dev.off()
